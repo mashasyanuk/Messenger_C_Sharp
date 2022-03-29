4544558924adb4a7e58a40f7e6f0e6b97dd41bf3
mashasyanuk <cool.super-masa@ya.ru>
2022-03-29 17:06:31 +0300

    Добавлен класс Messager, де/сериализация

--- Messenger_C/Program.cs
+++ Messenger_C/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Messenger_C
 {
+    [Serializable]
     public class Messenger
     {
 
--- Messenger_C/main.cs
+++ Messenger_C/main.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Messenger_C {
     class Program {
         static void Main(string[] args) {
 
-            Messenger message = new Messenger();
-            Console.WriteLine("Let's do it!");
-            Console.WriteLine(message);
+            Messenger message = new Messenger("mashasyanuk", "Good morning", DateTime.Now);
+            string output = JsonConvert.SerializeObject(message);
+            Console.WriteLine(output);
+            Messenger deserialize = JsonConvert.DeserializeObject<Messenger>(output);
+            Console.WriteLine(deserialize);
+
+//            Console.WriteLine("Let's do it!");
+ //           Console.WriteLine(message);
 
         }
     }
