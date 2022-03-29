a884713b2df9c29c9e823689d4a390dde434556c
m yan <61414925+mashasyanuk@users.noreply.github.com>
2022-03-29 18:03:09 +0300

    Update main.cs

--- Messenger_C/main.cs
+++ Messenger_C/main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 
 namespace Messenger_C {
@@ -6,13 +6,14 @@ namespace Messenger_C {
         static void Main(string[] args) {
 
             Messenger message = new Messenger("mashasyanuk", "Good morning", DateTime.Now);
+            Console.WriteLine("Let's do it!");
+            Console.WriteLine(message);
             string output = JsonConvert.SerializeObject(message);
+            Console.WriteLine("Serialized: ");
             Console.WriteLine(output);
             Messenger deserialize = JsonConvert.DeserializeObject<Messenger>(output);
+            Console.WriteLine("Deserialized: ");
             Console.WriteLine(deserialize);
-
-//            Console.WriteLine("Let's do it!");
- //           Console.WriteLine(message);
 
         }
     }
