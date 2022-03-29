00bfae05154f57c05a5198104e66a23923dbd060
mashasyanuk <cool.super-masa@ya.ru>
2022-03-29 16:54:15 +0300

    Created Class Message

---
+++ Messenger_C/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Messenger_C {
+    class Program {
+        static void Main(string[] args) {
+
+            Messenger message = new Messenger();
+            Console.WriteLine("Let's do it!");
+            Console.WriteLine(message);
+
+        }
+    }
+
+}
